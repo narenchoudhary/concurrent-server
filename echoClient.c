d9907a4e8609b7efa179468cc77aaf8c1269d315
narenchoudhary <narendra.prog@gmail.com>
2016-04-10 11:18:28 +0530

    added encode, decode to server, client

--- echoClient.c
+++ echoClient.c
@@ -9,20 +9,65 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define MAXLINE 4096 /*max text line length*/
+#define MAX_LEN 16 /*max text line length*/
+
+uint32_t * encode_msg(char *input){
+    uint32_t msg_type;
+
+    uint32_t *msg_full = (uint32_t *) malloc(sizeof(uint32_t) * (MAX_LEN + 2));
+
+    size_t msg_len;
+    size_t in_len;
+
+    msg_type = 1;
+    msg_full[0] = msg_type;
+
+    in_len = strlen(input);
+    while(in_len > 0 && (input[in_len-1] == '\n' || input[in_len-1] == '\r'))
+        input[--in_len] == 0;
+    msg_len = in_len;
+    msg_full[1] = (uint32_t) msg_len;
+
+    uint32_t i = 0;
+    for(i = 0; i < sizeof(input); i++){
+        msg_full[i+2] = htonl((uint32_t) input[i]);
+    }
+
+    printf("Message type is: %u\n", msg_type);
+    printf("Length of message is: %u\n", (uint32_t) msg_len);
+    printf("Encoded message is:");
+    for(i = 0; i < MAX_LEN +2 ; i++) {
+        printf("%u", msg_full[i]);
+    }
+    printf("\n");
+    return msg_full;
+}
+
+char *decode_msg(uint32_t *encoded_msg){
+    char *ret_str = malloc(sizeof(char) * MAX_LEN);
+
+    int i = 0;
+    for(i = 2; i < MAX_LEN+2; i++){
+        ret_str[i-2] = (char) ntohl(encoded_msg[i]);
+    }
+
+    printf("Decoded message is: %s\n", ret_str);
+    return ret_str;
+}
+
 
 int main(int argc, char **argv)
 {
-    int sockfd;
+    int sock_fd;
     struct sockaddr_in servaddr;
-    char input[MAXLINE], output[MAXLINE];
+    char input[MAX_LEN], output[MAX_LEN+2];
 
     if (argc != 3) {
       perror("Format: TCPClient < IP address of the server < Port of the server.");
       exit(1);
     }
 
-    if ((sockfd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
+    if ((sock_fd = socket (AF_INET, SOCK_STREAM, 0)) <0) {
       perror("Problem in creating the socket");
       exit(2);
     }
@@ -31,30 +76,53 @@ int main(int argc, char **argv)
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr= inet_addr(argv[1]);
-    servaddr.sin_port =  htons(atoi(argv[2])); //convert to big-endian order
+    servaddr.sin_port =  htons((uint16_t)atoi(argv[2]));
 
-    if (connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
+    if (connect(sock_fd, (struct sockaddr *) &servaddr, sizeof(servaddr))<0) {
       perror("Problem in connecting to the server");
       exit(3);
     }
 
     while (1) {
-      fgets(input, MAXLINE, stdin);
+        fgets(input, MAX_LEN, stdin);
 
-      send(sockfd, input, strlen(input), 0);
+        uint32_t *encoded_msg = encode_msg(input);
 
-      int len = recv(sockfd, output, MAXLINE, 0);
-      if(len < 0){
-        perror("read");
-        exit(-1);
-      }
-      if (len == 0){
-         perror("The server terminated prematurely");
-         exit(4);
-      }
-      output[len] = '\0';
-      printf("String received from the server: %s\n", output);
+        int i = 0;
+        printf("Encoded msg is:");
+        for(i = 0; i < MAX_LEN+2; i++){
+            printf("%u", encoded_msg[i]);
+        }
+        printf("\n");
+
+        size_t input_size = sizeof(uint32_t)*(MAX_LEN+2);
+
+        ssize_t bytes_sent = send(sock_fd, input, input_size, 0);
+        printf("Bytes sent:%zu\n",bytes_sent);
+
+        ssize_t bytes_received = recv(sock_fd, output, input_size, 0);
+        if(bytes_received > 0){
+
+            printf("Encoded msg received is:");
+            for(i = 0; i < MAX_LEN+2;i++){
+                printf("%u", output[i]);
+            }
+            printf("\n");
+
+            char *decoded_msg = decode_msg(output);
+            printf("Decoded message is:");
+            for(;*decoded_msg != NULL;++decoded_msg){
+                printf("%c", (unsigned char) decoded_msg);
+            }
+        }
+        else if(bytes_received < 0){
+            perror("read");
+            exit(-1);
+        }
+        else if (bytes_received == 0){
+            perror("The server terminated prematurely");
+            exit(4);
+        }
     }
-
     exit(0);
 }
--- server.c
+++ server.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <strings.h>
 #include <string.h>
@@ -8,30 +10,74 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include  <fcntl.h>
 
-#define MAXLINE 4096
+
 #define LISTENQ 8
 #define ERROR -1
 #define MAXCLIENTS 10
+#define MAX_LEN 16
 
-int main(int argc, char **argv){
+uint32_t * encode_msg(char *input){
+    uint32_t msg_type;
+    uint32_t msg_len;
+    uint32_t *msg_full = (uint32_t *) malloc(sizeof(uint32_t) * (MAX_LEN + 2));
+
+    size_t in_len;
+
+    msg_type = 1;
+    msg_full[0] = msg_type;
+
+    in_len = strlen(input);
+    while(in_len > 0 && (input[in_len-1] == '\n' || input[in_len-1] == '\r'))
+        input[--in_len] == 0;
+    msg_len = (uint32_t) in_len;
+    msg_full[1] = msg_len;
+
+    uint32_t i = 0;
+    for(i = 0; i < sizeof(input); i++){
+        msg_full[i+2] = htonl((uint32_t) input[i]);
+    }
+
+    printf("Message type is: %u\n", msg_type);
+    printf("Length of message is: %u\n", msg_len);
+    printf("Encoded message is:");
+    for(i = 0; i < MAX_LEN +2 ; i++) {
+        printf("%u", msg_full[i]);
+    }
+    printf("\n");
+    return msg_full;
+}
+
+char *decode_msg(uint32_t *encoded_msg, size_t data_len){
+    char *ret_str = malloc(sizeof(char) * data_len);
+
+    int i = 0;
+    for(i = 2; i < data_len; i++){
+        ret_str[i-2] = (char) ntohl(encoded_msg[i]);
+    }
+    return ret_str;
+}
+
+uint32_t main(uint32_t argc, char **argv){
     int listenfd, // listening socket fd
         connfd, // new connection socket fd
         setlistenfd, // listening socket fd opts
         true_int, // listening socket fd opts int value
-        n; // no of bytes received 
+        n; // no of bytes received
+
+    uint32_t ports[10] = {3002, 3003, 3004, 3005, 3006, 3007, 3008, 3009, 3010, 3011};
+
     pid_t childpid;
     socklen_t clilen;
-    // char buf[MAXLINE];
-    char data[MAXLINE];
-    // socket addr var for server
+
+    uint32_t data[MAX_LEN+2];
+    // socket address var for server
     struct sockaddr_in servaddr;
-    // socket addr var for client
+    // socket address var for client
     struct sockaddr_in cliaddr;
-    int sockaddr_len = sizeof(struct sockaddr_in);
+    uint32_t sockaddr_len = sizeof(struct sockaddr_in);
     // counter for clients
-    int active_clients = 0;
+    uint32_t active_clients = 0;
 
     if (argc != 2) {
       perror("Format: Server < Port of the server.");
@@ -44,9 +90,9 @@ int main(int argc, char **argv){
         perror("socket");
         exit(-1);
     }
-    
+
     true_int = 1;
-    setlistenfd = setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&true_int,sizeof(int));
+    setlistenfd = setsockopt(listenfd,SOL_SOCKET,SO_REUSEADDR,&true_int,sizeof(uint32_t));
     if(setlistenfd == ERROR){
         perror("setsockopt");
         exit(-1);
@@ -54,7 +100,7 @@ int main(int argc, char **argv){
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(atoi(argv[1]));
+    servaddr.sin_port = htons((uint16_t)atoi(argv[1]));
     bzero(&servaddr.sin_zero, 8);
 
     if(bind (listenfd, (struct sockaddr *) &servaddr, sockaddr_len) == ERROR){
@@ -75,7 +121,7 @@ int main(int argc, char **argv){
         connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &sockaddr_len);
 
         if(connfd == ERROR){
-            printf("Error while accepting the conncection\n");
+            printf("Error while accepting the connection\n");
             close(connfd);
             continue;
         }
@@ -88,29 +134,32 @@ int main(int argc, char **argv){
             // close listening socket
             close (listenfd);
 
-            int data_len = 1;
+            ssize_t data_len = 1;
 
             while(data_len){
 
-                data_len = recv(connfd, data, MAXLINE, 0);
+                size_t input_size = (MAX_LEN+2)*sizeof(uint32_t);
+
+                data_len = recv(connfd, data, input_size, 0);
+
+                int i = 0;
+                printf("Number of bytes read:%zu", data_len);
+                printf("Encoded data read is:");
+                for(i = 0; i < data_len/4; i++){
+                    printf("%u", data[i]);
+                }
+                printf("\n");
 
                 if(data_len){
-                    send(connfd, data, data_len, 0);
-                    data[data_len] = '\0';
-                    printf("String received from and resent to the client:%s\n", data);
+                    printf("Decoded message is:%s", decode_msg(data, data_len/4));
+                    printf("Bytes resent are: %zu\n", data_len);
+                    send(connfd, data, (size_t) data_len, 0);
                 }else{
                     break;
                 }
             }
-            
-            if(data_len == ERROR){
-                perror("Read error");
-                exit(-1);
-            }
-            else if(data_len == 0){
-                printf("Client closed connection.");
-                close(connfd);
-            }
+
+            close(connfd);
 
             active_clients--;
         }
